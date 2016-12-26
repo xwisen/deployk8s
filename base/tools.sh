626d78120ce0379102710d167c89244fae3c9ff0
xwisen <1031649164@qq.com>
2016-12-26 17:49:40 +0800

    Modify func yamltojson

--- base/tools.sh
+++ base/tools.sh
@@ -85,15 +85,23 @@ function yamltojson () {
 	python -c '
 import sys, yaml, json;
 json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)
-'
-< $1 > $2
+'< $1 > $2
+	if [[ $? -ne 0  ]];then
+		echo ">>>>>>convert failed !"
+	else
+		echo ">>>>>>convert succeed ! input is : $1, output is : $2"
+	fi
 }
 
 #eg: jsontoyaml test.json test.yaml
-function yamltojson () {
+function jsontoyaml () {
 	python -c '
 import sys, yaml, json;
-yaml.dump(json.load(sys.stdin), sys.stdout, indent=4)
-'
-< $1 > $2
+yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False)
+'< $1 > $2
+	if [[ $? -ne 0  ]];then
+		echo ">>>>>>convert failed !"
+	else
+		echo ">>>>>>convert succeed ! input is : $1, output is : $2"
+	fi
 }
